1697cace9be2477933d969d508a3b265d14eba51
Aren Bruce <devtodaydevtomorrow@gmail.com>
2026-02-16 18:11:47 -0500

    minimal routing with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardHomeComponent } from '././dashboard/dashboard-home/dashboard-home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'login', loadComponent: () => import('./auth/login/login.component') }
+    { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
+    {path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent)},
+    {path: 'dashboard', loadComponent: () => import('././dashboard/dashboard-home/dashboard-home.component').then(m => m.DashboardHomeComponent)},
+    {path: '**', redirectTo: 'dashboard'}
 ];
