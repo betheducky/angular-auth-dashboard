7cfeb3462902be679e9f9b1ab542e2dff8113f2d
Aren Bruce <devtodaydevtomorrow@gmail.com>
2026-02-16 13:03:30 -0500

    correction to app structure as standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'login', loadComponent: () => import('./auth/login/login.component') }
+];
