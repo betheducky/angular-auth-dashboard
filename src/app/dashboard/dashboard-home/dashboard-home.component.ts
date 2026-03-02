bcdbdaa62544a32da9ca6f015e3cc87dd1d78516
Aren Bruce <devtodaydevtomorrow@gmail.com>
2026-03-02 12:55:22 -0500

    UI display of dashboard data

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,28 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../core/service/dashboard.service';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { User } from '../../models/user.model';
 import { AuthService } from '../../core/service/auth.service';
+import { DashboardData } from '../../models/dashbard.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AsyncPipe],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.scss'
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit{
 
   currentUser: User | null = null;
+  dashboardData$!: Observable<DashboardData>;
+
+  ngOnInit(){
+   this.dashboardData$ = this.dashboardService.getDashboardData();
+   console.log('Current user is:', this.currentUser);
+   console.log('Dashboard data is: ', this.dashboardData$ ? 'PRESENT!' : 'MISSING!');
+  }
+
 
   constructor(private dashboardService: DashboardService, private authService: AuthService){
     this.authService.currentUser$.subscribe(user => {
       this.currentUser = user;
     });
-
-    if(this.currentUser) {
-      this.dashboardService.getDashboardData();
-    }
   }
 
 }
